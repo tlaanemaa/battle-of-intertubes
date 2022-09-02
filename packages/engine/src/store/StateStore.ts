53a88baeadc355b59985607d8d26e50427d9f477
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2022-09-03 00:10:09 +0200

    Added a physics based moose

--- packages/engine/src/store/StateStore.ts
+++ packages/engine/src/store/StateStore.ts
@@ -1,25 +1,12 @@
 import { singleton } from "tsyringe";
 import { Entity } from "../core/Entity";
+import { Moose } from "../entities/Moose";
 
-const mooseImg = new Image();
-mooseImg.src =
-  "https://images.vexels.com/media/users/3/227446/isolated/lists/7867873566b6dda4db49b5d752009b07-cute-moose-flat.png";
-
-  const moose: Entity = {
-    texture: mooseImg,
-    height: 20,
-    width: 40,
-    x: 0,
-    y: 0
-  }
+const mockEntities = [new Moose(), new Moose(), new Moose()];
 
 @singleton()
 export class StateStore {
   public getStateForRendering(): Entity[] {
-
-    moose.x = moose.x + (Math.random() - 0.5) * 10
-    moose.y = moose.y + (Math.random() - 0.5) * 10
-
-    return [moose];
+    return mockEntities;
   }
 }
