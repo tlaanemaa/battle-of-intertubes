852da97eac38f6d76d78d4b3dd15ab08560b5c9c
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2022-09-03 15:18:58 +0200

    Tweaked moose

--- packages/engine/src/entities/Moose.ts
+++ packages/engine/src/entities/Moose.ts
@@ -1,22 +1,22 @@
 import { PhysicalRenderObject } from "../core/PhysicalRenderObject";
 
 export class Moose extends PhysicalRenderObject {
-  public readonly texture: CanvasImageSource;
-  public readonly height = 20;
-  public readonly width = 40;
+  public readonly texture = new Image();
+  public readonly height = 200;
+  public readonly width = 200;
 
   constructor() {
     super();
-    this.texture = new Image();
     this.texture.src =
       "https://images.vexels.com/media/users/3/227446/isolated/lists/7867873566b6dda4db49b5d752009b07-cute-moose-flat.png";
 
-    const forceBound = 10000;
-    setInterval(() => {
-      this.applyForce({
-        x: Math.random() * 2 * forceBound - forceBound,
-        y: Math.random() * 2 * forceBound - forceBound,
-      });
-    }, 100);
+    setInterval(() => this.tick(100000), 1000);
+  }
+
+  tick(forceBound = 2000) {
+    this.applyForce({
+      x: Math.random() * 2 * forceBound - forceBound,
+      y: Math.random() * 2 * forceBound - forceBound,
+    });
   }
 }
--- packages/engine/src/store/StateStore.ts
+++ packages/engine/src/store/StateStore.ts
@@ -2,7 +2,7 @@ import { singleton } from "tsyringe";
 import { PhysicalRenderObject } from "../core/PhysicalRenderObject";
 import { Moose } from "../entities/Moose";
 
-const mockEntities = [new Moose(), new Moose(), new Moose()];
+const mockEntities = new Array(5000).fill(1).map(() => new Moose());
 
 @singleton()
 export class StateStore {
