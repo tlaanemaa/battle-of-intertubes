5d71daaf182034a7628cc0311711ed3b268ebfff
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2022-09-04 12:56:37 +0200

    Added a wonky background

--- packages/web/src/ts/Background.ts
+++ packages/web/src/ts/Background.ts
@@ -1,25 +1,61 @@
-import { Object2D } from "@battle-of-intertubes/engine";
 import { singleton } from "tsyringe";
+import { Object2D } from "@battle-of-intertubes/engine";
 
+// TODO: Fix this class its too hacky
 @singleton()
 export class Background {
+  private backgroundImage = new Image();
   private readonly imageSize = 400;
-  private readonly element = document.getElementById(
+  private readonly canvasElement = document.getElementById(
     "game-background"
-  ) as HTMLDivElement;
+  ) as HTMLCanvasElement;
 
-  public setPosition(position: Object2D, zoomModifier: number) {
-    const x = position.x % this.imageSize;
-    const y = position.y % this.imageSize;
-    this.setSize(zoomModifier);
-    this.element.style.transform = `translate(${x}px, ${y}px) scale(${zoomModifier})`;
+  constructor() {
+    this.backgroundImage.onload = () => this.setPosition({ x: 0, y: 0 }, 1);
+    this.backgroundImage.src = "img/bananas.webp";
+    window.addEventListener("resize", this.resizeCanvas.bind(this));
+    this.resizeCanvas();
   }
 
-  private setSize(zoomModifier: number) {
-    const scale = 1 / zoomModifier;
-    this.element.style.top = `${-this.imageSize * 1.1 * scale}px`;
-    this.element.style.left = `${-this.imageSize * 1.1 * scale}px`;
-    this.element.style.bottom = `${-this.imageSize * 1.1 * scale}px`;
-    this.element.style.right = `${-this.imageSize * 1.1 * scale}px`;
+  private resizeCanvas() {
+    this.canvasElement.height = window.innerHeight;
+    this.canvasElement.width = window.innerWidth;
+    this.setPosition({ x: 0, y: 0 }, 1);
+  }
+
+  public setPosition(position: Object2D, zoomModifier: number) {
+    this.drawBackground(position, zoomModifier);
+  }
+
+  /**
+   * Draws the background to the canvas
+   */
+  public drawBackground(position: Object2D, zoomModifier: number) {
+    const renderX = (position.x % this.imageSize) - this.imageSize;
+    const renderY = (position.y % this.imageSize) - this.imageSize;
+
+    const imageCanvas = document.createElement("canvas");
+    imageCanvas.height = this.imageSize * zoomModifier;
+    imageCanvas.width = this.imageSize * zoomModifier;
+    const tCtx = imageCanvas.getContext("2d")!;
+    tCtx.drawImage(
+      this.backgroundImage,
+      0,
+      0,
+      imageCanvas.width,
+      imageCanvas.height
+    );
+
+    const ctx = this.canvasElement.getContext("2d")!;
+    ctx.fillStyle = ctx.createPattern(imageCanvas, "repeat")!;
+    ctx.translate(Math.round(renderX), Math.round(renderY));
+    ctx.fillRect(
+      0,
+      0,
+      Math.round(this.canvasElement.width - renderX),
+      Math.round(this.canvasElement.height - renderY)
+    );
+    // Reset current transformation matrix to the identity matrix
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
   }
 }
