ab6c30c304e02736a80f5ce925aeb69418dbaf19
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2022-09-04 19:41:28 +0200

    Fixed some zoom issues

--- packages/web/src/ts/Background.ts
+++ packages/web/src/ts/Background.ts
@@ -12,7 +12,8 @@ export class Background {
 
   constructor() {
     this.backgroundImage.onload = () => this.setPosition({ x: 0, y: 0 }, 1);
-    this.backgroundImage.src = "img/bananas.webp";
+    this.backgroundImage.src =
+      "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png";
     window.addEventListener("resize", this.resizeCanvas.bind(this));
     this.resizeCanvas();
   }
@@ -31,31 +32,49 @@ export class Background {
    * Draws the background to the canvas
    */
   public drawBackground(position: Object2D, zoomModifier: number) {
-    const renderX = (position.x % this.imageSize) - this.imageSize;
-    const renderY = (position.y % this.imageSize) - this.imageSize;
+    const scaledImageSize = this.imageSize * zoomModifier;
 
-    const imageCanvas = document.createElement("canvas");
-    imageCanvas.height = this.imageSize * zoomModifier;
-    imageCanvas.width = this.imageSize * zoomModifier;
-    const tCtx = imageCanvas.getContext("2d")!;
-    tCtx.drawImage(
-      this.backgroundImage,
-      0,
-      0,
-      imageCanvas.width,
-      imageCanvas.height
-    );
+    const renderX = (position.x % scaledImageSize) - scaledImageSize;
+    const renderY = (position.y % scaledImageSize) - scaledImageSize;
+    const renderWidth = this.canvasElement.width - renderX;
+    const renderHeight = this.canvasElement.height - renderY;
 
     const ctx = this.canvasElement.getContext("2d")!;
-    ctx.fillStyle = ctx.createPattern(imageCanvas, "repeat")!;
-    ctx.translate(Math.round(renderX), Math.round(renderY));
+    ctx.translate(
+      Math.round(renderX + this.canvasElement.width / 2),
+      Math.round(renderY + this.canvasElement.height / 2)
+    );
+
+    ctx.fillStyle = ctx.createPattern(
+      this.getBackgroundImage(scaledImageSize),
+      "repeat"
+    )!;
+
     ctx.fillRect(
-      0,
-      0,
-      Math.round(this.canvasElement.width - renderX),
-      Math.round(this.canvasElement.height - renderY)
+      Math.round(-this.canvasElement.width / 2),
+      Math.round(-this.canvasElement.height / 2),
+      Math.round(renderWidth),
+      Math.round(renderHeight)
     );
     // Reset current transformation matrix to the identity matrix
     ctx.setTransform(1, 0, 0, 1, 0, 0);
+  }
+
+  private getBackgroundImage(backgroundSize: number) {
+    const imageCanvas = document.createElement("canvas");
+    imageCanvas.height = backgroundSize;
+    imageCanvas.width = backgroundSize;
+
+    imageCanvas
+      .getContext("2d")!
+      .drawImage(
+        this.backgroundImage,
+        0,
+        0,
+        imageCanvas.width,
+        imageCanvas.height
+      );
+
+    return imageCanvas;
   }
 }
