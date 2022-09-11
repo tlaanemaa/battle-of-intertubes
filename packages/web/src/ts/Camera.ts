450c827c4ad10d730656ce28dbac98b518fa146d
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2022-09-11 19:34:58 +0200

    Added some limits on zooming to reduce lag

--- packages/web/src/ts/Camera.ts
+++ packages/web/src/ts/Camera.ts
@@ -2,5 +2,13 @@ import { Object2D } from "@battle-of-intertubes/core";
 
 export class Camera {
   public position: Object2D = { x: 0, y: 0 };
-  public zoom: number = 1;
+  private _zoom: number = 1;
+
+  public get zoom() {
+    return this._zoom;
+  }
+
+  public set zoom(zoom: number) {
+    this._zoom = Math.max(0.005, Math.min(50, zoom));
+  }
 }
