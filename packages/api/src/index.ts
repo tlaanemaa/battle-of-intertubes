f50b151470977e5ad179145a01c7bd06932f50e9
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2022-09-21 19:59:09 +0200

    Added a startup message

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,7 +3,9 @@ import { WebSocketServer } from "ws";
 import { Message } from "@battle-of-intertubes/core";
 
 const PORT = parseInt(process.env.PORT!) || 8080;
-const wss = new WebSocketServer({ port: PORT });
+const wss = new WebSocketServer({ port: PORT }, () =>
+  console.log(`Websocket server listening at ws://localhost:${PORT}`)
+);
 
 wss.on("connection", (ws) => {
   ws.on("message", (data) => {
