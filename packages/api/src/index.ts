c82b5fb6e8c3ae03aea06a48ad50fa5c2e8ea0d6
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2022-10-03 20:21:09 +0200

    Removed unnecessary import

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata";
 import { container } from "tsyringe";
-import "@moose-rocket/game";
 import { SocketServer } from "./server/SocketServer";
 
 container.resolve(SocketServer);
